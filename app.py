15b6207b66e38b2215c3fac7ca0fc9c7a26ba4be
Zachary Petro <zmpetro@gmail.com>
2022-01-20 20:03:47 +0000

    initial flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    message = request.get_json()
+    print(message)
+    return "ok", 200
