d08b34ea1fc0129bdb34ec8d01ffc4def50dc2b6
Zachary Petro <zmpetro@gmail.com>
2022-01-20 23:56:18 +0000

    regex to check if message is wordle score

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import os
 import json
+import re
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 from flask import Flask, request
+
+def is_wordle_message(message: str) -> bool:
+    found = re.search("^Wordle\s\d+\s[1-5X]\/\d", message)
+    if (found):
+        return True
+    else:
+        return False
 
 app = Flask(__name__)
 
@@ -12,4 +20,7 @@ app = Flask(__name__)
 def webhook():
     message = request.get_json()
     print(message)
-    return "ok", 200
+    if(is_wordle_message(message['text']) == False):
+        return "ok", 200
+    # Message is a Wordle message
+
